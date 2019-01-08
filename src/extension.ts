678e4d9639fd7898cdc0334cfe324d98d0a02465
amir <amiralizadeh9480@gmail.com>
2019-01-08 21:50:34 +0330

    Better route autocomplete; route parameters autocomplete added.

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 
-var routes: Array<string> = [];
+var routes: Array<any> = [];
 
 function exec(command: string, options: cp.ExecOptions): Promise<{ stdout: string; stderr: string }> {
 	return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
@@ -26,11 +26,13 @@ async function loadRoutes() {
 
 			routes = [];
 			for (var i in lines) {
-				var line:any = lines[i].split(" | ").splice(3, 1);
-				if (line.length > 0) {
-					line = line[0].trim();
-					if (line.length > 0) {
-						routes.push(line);
+				var line = lines[i].split(" | ");
+				if (line.length >= 3) {
+					for (i in line) {
+						line[i] = line[i].trim();
+					}
+					if (line[3].length > 0) {
+						routes.push({method: line[1], uri: line[2], route: line[3], action: line[4]});
 					}
 				}
 			}
@@ -55,14 +57,42 @@ export function activate(context: vscode.ExtensionContext) {
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
-				let linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.includes("route")) {
+				let linePrefix = document.lineAt(position).text.substr(0, position.character).toLowerCase().trim();
+				if (! linePrefix.includes("route")) {
 					return undefined;
 				}
 
 				var out:Array<vscode.CompletionItem> = [];
+
+				if (linePrefix.substr(linePrefix.indexOf("route") + 5).includes(",")) {
+					// route parameters autocomplete
+					var currentRoute = (/\(('|")([A-Za-z0-9_-]+)('|")/g).exec(linePrefix);
+					if (currentRoute != null && currentRoute[2].length > 0) {
+						for (var i in routes) {
+							if (routes[i].route == currentRoute[2]) {
+								var routeParamRegex = (/\{([A-Za-z0-9_]+)(\?|)\}/g);
+								var currentRouteParameters:any = routeParamRegex.exec(routes[i].uri);
+								while (currentRouteParameters != null) {
+									if (currentRouteParameters != null && currentRouteParameters[1].length > 0) {
+										var compeleteItem = new vscode.CompletionItem(currentRouteParameters[1], vscode.CompletionItemKind.Variable);
+										compeleteItem.detail = currentRouteParameters[1];
+										out.push(compeleteItem);
+									}
+									currentRouteParameters = routeParamRegex.exec(routes[i].uri);
+								}
+								return out;
+							}
+						}
+					}
+				}
+
+				// route name autocomplete
 				for (var i in routes) {
-					out.push(new vscode.CompletionItem(routes[i], vscode.CompletionItemKind.Text));
+					var compeleteItem = new vscode.CompletionItem(routes[i].route, vscode.CompletionItemKind.Enum);
+					compeleteItem.detail = routes[i].action.replace(/App\\Http\\Controllers\\/g, '') +
+											"\n\n" +
+											routes[i].method.replace(/\|HEAD|HEAD\|/g, '') + ":" + routes[i].uri;
+					out.push(compeleteItem);
 				}
 				return out;
 			}
